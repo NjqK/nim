25508cd56c21382093519e40c12d162e5aab5b39
kuro <770629348@qq.com>
2020-05-07 12:52:27 +0800

    bug fixes

--- chat/src/main/java/com/example/chat/ChatApplication.java
+++ chat/src/main/java/com/example/chat/ChatApplication.java
@@ -1,6 +1,9 @@
 package com.example.chat;
 
+import com.example.common.redis.JedisUtil;
+import com.example.common.zk.ZkUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +14,43 @@ import javax.annotation.PreDestroy;
 @SpringBootApplication
 public class ChatApplication {
 
+    @Value("${zookeeper.url}")
+    private String zkUrl;
+
+    @Value("${redis.pool.host}")
+    private String redisHost;
+
+    @Value("${redis.pool.port}")
+    private String redisPort;
+
+    @Value("${redis.pool.password}")
+    private String redisPassword;
+
+    @Value("${redis.pool.maxTotal}")
+    private String redisMaxTotal;
+
+    @Value("${redis.pool.maxIdle}")
+    private String redisMaxIdle;
+
+    @Value("${redis.pool.maxWaitMillis}")
+    private String redisMaxWaitMillis;
+
     @PostConstruct
     private void onStart() {
+        initZk();
+        initRedis();
+    }
 
+    private void initRedis() {
+        int port = Integer.parseInt(redisPort);
+        int maxTotal = Integer.parseInt(redisMaxTotal);
+        int maxIdle = Integer.parseInt(redisMaxIdle);
+        int maxWait = Integer.parseInt(redisMaxWaitMillis);
+        JedisUtil.initJedisUtil(redisHost, port, redisPassword, maxTotal, maxIdle, maxWait);
+    }
+
+    private void initZk() {
+        ZkUtil.start(zkUrl, null);
     }
 
     @PreDestroy
